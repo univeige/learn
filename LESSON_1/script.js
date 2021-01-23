6b8664b9f0ac5a30f8754b870ee59b76aaa64395
univeige <kuryaev@gmail.com>
2021-01-24 01:51:05 +0300

    Исправлен синтаксис

--- LESSON_1/script.js
+++ LESSON_1/script.js
@@ -17,15 +17,15 @@ for ( let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
         b = +prompt("Во сколько обойдется?", '');
     
-    if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
-    && a != '' && b != '' && a.length < 50 ) {
+    if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null &&
+    a != '' && b != '' && a.length < 50 ) {
         
         console.log("Done!");
         appData.expenses[a] = b;
     } else {
         i--;
     }
-};
+}
 
 appData.moneyPerDay =  appData.budget / 30;
 
